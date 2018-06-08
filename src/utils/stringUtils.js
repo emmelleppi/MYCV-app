539915cba1889858221f18126be192ae6d02cc52
Marco Ludovico Perego <mlp.perego@gmail.com>
2018-06-08 02:43:51 +0200

    :recycle: refactoring to validate strings with more stable npm packages

--- src/utils/stringUtils.js
+++ src/utils/stringUtils.js
@@ -1,14 +1,15 @@
+import emailValidator from 'validator/lib/isEmail';
+import phoneValidator from 'validator/lib/isMobilePhone';
+
 export function toTitleCase(title) {
-  return title
-    .split(/ /g)
-    .map((word) => `${word.substring(0, 1).toUpperCase()}${word.substring(1).toLowerCase()}`)
-    .join(' ')
+  const startCase = require('lodash/startcase')
+  return startCase(title)
 }
 
 export function isPhoneNumber(number) {
-  return !number || number.match(/^([+]39)?([\d]{6,12})$/)
+    return phoneValidator(number,'it-IT')
 }
 
 export function isEmail(email) {
-  return !email || email.match(/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/)
+  return emailValidator(email)
 }
